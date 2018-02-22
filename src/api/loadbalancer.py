403553695ee7e649840ddf862513fa99175cd414
Daniel Treiman <dtreiman@alivecor.com>
2018-02-22 14:02:50 -0800

    Removed duplicate qhost and qstat calls.

--- src/api/loadbalancer.py
+++ src/api/loadbalancer.py
@@ -94,9 +94,7 @@ class LoadBalancer:
         idle_hosts = self._idle_hosts
         # print('Checking for idle hosts at time %.1f.' % time_now)
         # Check to see if any hosts are idle.
-        hosts = sge.qhost()
         host_names = set([h['name'] for h in hosts if not 'master' in ['name']])
-        queued_jobs, _ = sge.qstat()
         # Get hosts with running jobs.
         busy_hosts = set([j['queue_name'].split('@')[1] for j in queued_jobs])
         # Remove hosts with jobs from idle list
